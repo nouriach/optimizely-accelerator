4c8b6cbcd01d8af873d8f40970753191f8bb46d2
nouriach <nathan.ouriach@deptagency.com>
2022-03-11 10:36:21 +0000

    set up architecture

---
+++ Optimizely.Accelerator/Controllers/Base/BasePageController.cs
@@ -0,0 +1,27 @@
+﻿using EPiServer.Core;
+using EPiServer.Web.Mvc;
+using Optimizely.Accelerator.Interfaces;
+using System;
+
+namespace Optimizely.Accelerator.Web.Controllers.Base
+{
+    public class BasePageController<T> : PageController<T> where T : PageData
+    {
+        private readonly ILogger _logger;
+
+        public BasePageController(ILogger logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        internal void JsonDebugCheck()
+        {
+            if (Request != null && Request.Query["debug"] == "serverjson")
+            {
+                _logger.LogDebug(nameof(Index), "Enabling JSON");
+
+                ViewBag.DebugJson = "serverjson";
+            }
+        }
+    }
+}
