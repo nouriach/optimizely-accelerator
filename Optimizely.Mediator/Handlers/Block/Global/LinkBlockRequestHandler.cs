1381071c3ffe7c9f3f30cbe28aff612b00e0862a
nouriach <nathan.ouriach@deptagency.com>
2022-03-14 17:07:31 +0000

    Create BlockRequest & LinkBlockRequestHandler

---
+++ Optimizely.Mediator/Handlers/Block/Global/LinkBlockRequestHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Optimizely.Interfaces;
+using Optimizely.Mediator.Requests.Block;
+using Optimizely.Models.Blocks.Global;
+using Optimizely.ViewModels.Blocks.Interfaces;
+using Optimizely.ViewModels.Global.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Optimizely.Mediator.Handlers.Block.Global
+{
+    public class LinkBlockRequestHandler : IRequestHandler<BlockRequest<LinkBlock>, ISiteBlockViewModel>
+    {
+        private readonly IUrlHelper _urlHelper;
+
+        public LinkBlockRequestHandler(IUrlHelper urlHelper)
+        {
+            _urlHelper = urlHelper;
+        }
+        public async Task<ISiteBlockViewModel> Handle(BlockRequest<LinkBlock> request, CancellationToken cancellationToken)
+        {
+            var block = request?.Block;
+            var url = _urlHelper.GetTidyUrl(block?.Url);
+
+            return new LinkBlockViewModel
+            {
+                Name = block?.Name,
+                Target = block?.Target,
+                Url = url
+            };
+        }
+    }
+}
---
+++ Optimizely.Mediator/Requests/Block/BlockRequest.cs
@@ -0,0 +1,24 @@
+﻿using EPiServer.Core;
+using MediatR;
+using Optimizely.ViewModels.Blocks.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimizely.Mediator.Requests.Block
+{
+    public class BlockRequest<T> : IRequest<ISiteBlockViewModel> where T : BlockData
+    {
+        public BlockRequest(T block, CultureInfo language)
+        {
+            Block = block;
+            Language = language;
+        }
+
+        public T Block { get; set; }
+        public CultureInfo Language { get; set; }
+    }
+}
