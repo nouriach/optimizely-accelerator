2b6c5a989c73b645577a796f28877a25ecef471c
nouriach <nathan.ouriach@deptagency.com>
2022-03-15 15:43:18 +0000

    Build NavigationService method to get homepage

--- Optimizely.Interfaces/INavigationService.cs
+++ Optimizely.Interfaces/INavigationService.cs
@@ -1,8 +1,10 @@
-﻿
+﻿using EPiServer.Core;
+using Optimizely.ViewModels.Blocks.Components;
+
 namespace Optimizely.Interfaces
 {
     public interface INavigationService
     {
-
+        NavigationItem GetHomepage();
     }
 }
--- Optimizely.Services/NavigationService.cs
+++ Optimizely.Services/NavigationService.cs
@@ -1,8 +1,36 @@
-﻿using Optimizely.Interfaces;
+﻿using EPiServer;
+using EPiServer.Web.Routing;
+using Optimizely.Interfaces;
+using Optimizely.Models.Base;
+using Optimizely.ViewModels.Blocks.Components;
+using System.Linq;
 
 namespace Optimizely.Services
 {
     public class NavigationService : INavigationService
     {
+        private readonly IContentLoader _loader;
+        private readonly IUrlHelper _urlHelper;
+        private readonly IPageRouteHelper _pageRouteHelper;
+
+        public NavigationService(IContentLoader loader, IUrlHelper urlHelper, IPageRouteHelper pageRouteHelper)
+        {
+            _loader = loader;
+            _urlHelper = urlHelper;
+            _pageRouteHelper = pageRouteHelper;
+        }
+
+        public NavigationItem GetHomepage()
+        {
+            var currentPage = _pageRouteHelper.Page.ContentLink;
+            var homepage = _loader.GetAncestors(currentPage).OfType<SitePageData>().Last();
+
+            return new NavigationItem
+            {
+                Title = homepage?.Name,
+                Url = _urlHelper.GetTidyUrl(homepage),
+                PageData = homepage
+            };
+        }
     }
 }
