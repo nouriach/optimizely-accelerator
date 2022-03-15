1f3f2832087a1d00a65b6fde78fe6132de33c5ca
nouriach <nathan.ouriach@deptagency.com>
2022-03-15 12:22:50 +0000

    Create helper for getting block CultureInfo

---
+++ Optimizely.Core/Helpers/CultureInfoHelper.cs
@@ -0,0 +1,13 @@
+﻿using EPiServer.Core;
+using System.Globalization;
+
+namespace Optimizely.Core.Helpers
+{
+    public static class CultureInfoHelper
+    {
+        public static CultureInfo GetCultureInfoFromBlockLanguage(this BlockData currentBlock)
+        {
+            return new CultureInfo(currentBlock.Property.LanguageBranch);
+        }
+    }
+}
