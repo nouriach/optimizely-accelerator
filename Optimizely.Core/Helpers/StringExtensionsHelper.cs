90f90048278d4ce492c9758bac0c502b49398ac6
nouriach <nathan.ouriach@deptagency.com>
2022-03-16 10:55:10 +0000

    Create helper for adding ellipsis to string content

---
+++ Optimizely.Core/Helpers/StringExtensionsHelper.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Optimizely.Core.Helpers
+{
+    public static class StringExtensionsHelper
+    {
+        public static string MakeCompactString(this string str, int maxLength = 30, string suffix = "...")
+        {
+            var newStr = string.IsNullOrEmpty(str) ? string.Empty : str;
+            var strLength = string.IsNullOrEmpty(str) ? 0 : str.Length;
+            if (strLength > maxLength)
+            {
+                newStr = str?.Substring(0, maxLength);
+            }
+
+            return newStr + suffix;
+        }
+    }
+}
