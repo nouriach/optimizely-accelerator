9e1e5064ec056c8e15a96780dd6a940c941f696b
nouriach <nathan.ouriach@deptagency.com>
2022-03-16 10:49:35 +0000

    Add attribute to check min/max property values

---
+++ Dept.Core/Attributes/MinAndMaxElementsAttribute.cs
@@ -0,0 +1,26 @@
+﻿using EPiServer.Core;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dept.Core.Attributes
+{
+    public class MinAndMaxElementsAttribute : ValidationAttribute
+    {
+        private readonly int _minimumLimit;
+        private readonly int _maximumLimit;
+        public MinAndMaxElementsAttribute(int minLimit, int maxLimit)
+        {
+            _minimumLimit = minLimit;
+            _maximumLimit = maxLimit;
+        }
+        public override bool IsValid(object value)
+        {
+            return ValidateProperty(value as IList<ContentReference>);
+        }
+        private bool ValidateProperty(IList<ContentReference> item)
+        {
+            return item == null ||
+                item.Count >= _minimumLimit && item.Count <= _maximumLimit;
+        }
+    }
+}
