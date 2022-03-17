6af29c70c697f1db69fc484d7ecdc283ec0d10d5
nouriach <nathan.ouriach@deptagency.com>
2022-03-17 10:17:19 +0000

    Setup controller for ImageBlock

---
+++ Optimizely.Accelerator/Controllers/Blocks/ImageBlockController.cs
@@ -0,0 +1,39 @@
+﻿using EPiServer.Web.Mvc;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Optimizely.Core.Helpers;
+using Optimizely.Interfaces;
+using Optimizely.Mediator.Requests.Block;
+using Optimizely.Models.Blocks;
+using Optimizely.ViewModels.Blocks.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace Optimizely.Web.Controllers.Blocks
+{
+    public class ImageBlockController : AsyncBlockComponent<ImageBlock>
+    {
+        private readonly IMediator _mediator;
+        private readonly IProjectLogger<ImageBlockController> _logger;
+
+        public ImageBlockController(IMediator mediator, IProjectLogger<ImageBlockController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+        protected async override Task<IViewComponentResult> InvokeComponentAsync(ImageBlock currentContent)
+        {
+            var model = new ImageBlockViewModel();
+            try
+            {
+                model = await _mediator.Send(BlockRequest.Create(currentContent, currentContent.GetCultureInfoFromBlockLanguage()), default) as ImageBlockViewModel;
+            }
+            catch (Exception e)
+            {
+
+                _logger.LogError(nameof(Index), e, "Error ImageBlockController getting ImageBlockViewModel");
+            }
+            return View("~/Views/Shared/JsonDisplay.cshtml", model);
+        }
+    }
+}
--- Optimizely.Models/Media/ImageFile.cs
+++ Optimizely.Models/Media/ImageFile.cs
@@ -1,7 +1,11 @@
 ﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
 
 namespace Optimizely.Models.Media
 {
+    [ContentType(GUID = "98975083-8A6C-4A1E-A2BE-8AF060814D39")]
+    [MediaDescriptor(ExtensionString = "jpg,jpeg,png")]
     public class ImageFile : ImageData
     {
     }
